3f0a48517814046dd13af552cf657fa24af06bab
Elisha Meshida <etm81699@uga.edu>
2026-06-30 04:33:09 -0400

    Update MovieRepository.java
    Added status param

--- backend/src/main/java/com/cinema/ebooking/repository/MovieRepository.java
+++ backend/src/main/java/com/cinema/ebooking/repository/MovieRepository.java
@@ -2,17 +2,21 @@ package com.cinema.ebooking.repository;
 
 import com.cinema.ebooking.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.cinema.ebooking.entity.MovieStatus;
 
 import java.util.List;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
-    List<Movie> findByTitleContainingIgnoreCase(String title);
+List<Movie> findByStatus(MovieStatus status);
 
-    List<Movie> findByCategoryIgnoreCase(String category);
+List<Movie> findByStatusAndTitleContainingIgnoreCase(MovieStatus status, String title);
 
-    List<Movie> findByTitleContainingIgnoreCaseAndCategoryIgnoreCase(
-            String title,
-            String category
-    );
+List<Movie> findByStatusAndCategoryIgnoreCase(MovieStatus status, String category);
+
+List<Movie> findByStatusAndTitleContainingIgnoreCaseAndCategoryIgnoreCase(
+        MovieStatus status,
+        String title,
+        String category
+);
 }
